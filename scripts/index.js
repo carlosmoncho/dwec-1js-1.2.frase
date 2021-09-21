1d4ed318dea8be1906c01e10899f241b98c95eb0
Carlos Moncho Molla <carlosmoncho2002@gmail.com>
2021-09-21 11:34:41 +0200

    [ADD] dwec-1js-1.2.frase

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
 'use strict'
 let frase = prompt('introduce una cadena')
-console.log(letras(frase))
-console.log(palabras(frase))
-console.log(maysc(frase))
-console.log(titulo(frase))
-console.log(letrasReves(frase))
-console.log(palabrasReves(frase))
-if(palindromo(frase)){
-    console.log('Sí es un palíndromo')
-}else {
-    console.log('No es un palíndromo')
+if(frase === ""){
+    console.log('Debes introducir una cadena para que funcione')
+}else{
+    console.log(letras(frase))
+    console.log(palabras(frase))
+    console.log(maysc(frase))
+    console.log(titulo(frase))
+    console.log(letrasReves(frase))
+    console.log(palabrasReves(frase))
+    if(palindromo(frase)){
+        console.log('Sí es un palíndromo')
+    }else {
+        console.log('No es un palíndromo')
+    }
 }
